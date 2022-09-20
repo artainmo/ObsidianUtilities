5ee3b8d5c0c77f472eb1fe747e7af18cdc20761a
Arthur Tainmont <arthurtainmont@Arthurs-Mac-mini.local>
2022-09-20 21:35:14 +0200

    paste to clipboard auto

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php 
+function pasteToClipboard($x) {
+	echo '<script type="text/javascript">' .
+			'navigator.clipboard.writeText("' .
+			$x .
+		 '");</script>';
+}
+
 if (isset($_POST['tagToTitle'])) {
 	$newTitle = trim($_POST['tags']);
 	$newTitle = str_replace("#", "", $newTitle);
 	$newTitle = explode(' ', $newTitle);
 	usort($newTitle, 'strnatcasecmp');
 	$newTitle = implode(', ', $newTitle);
+	pasteToClipboard($newTitle);
 }
 
 if (isset($_POST['hideLink'])) {
@@ -12,6 +20,7 @@ if (isset($_POST['hideLink'])) {
 	$newLink = str_replace(["*", "#"], "", $newLink);
 	$newLink = explode(':', $newLink, 2);
 	$newLink = "[" . trim($newLink[0]) . "](" . trim($newLink[1]) . ")";
+	pasteToClipboard($newLink);
 }
 ?>
 
@@ -31,6 +40,7 @@ if (isset($_POST['hideLink'])) {
 	<form action="index.php" method="POST">
 		<label><strong>Transform tags to title</strong> 
 			<br><em>(expects this notation: #tag1 #tag2)</em>
+			<br><em>(automatically pastes to clipboard)</em>
 		</label>
 		<br/><br/>
 		<textarea name="tags" rows="6" cols="100" maxlength="500" required><?php if (isset($newTitle)) { echo $newTitle; } ?></textarea>
@@ -43,6 +53,7 @@ if (isset($_POST['hideLink'])) {
 	<form action="index.php" method="POST">
 		<label><strong>Hide link within text</strong> 
 			<br><em>(expects this notation: title: link)</em>
+			<br><em>(automatically pastes to clipboard)</em>
 		</label>
 		<br/><br/>
 		<textarea name="textLink" rows="6" cols="100" maxlength="500" required><?php if (isset($newLink)) { echo $newLink; } ?></textarea>
