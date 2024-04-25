c1bdd7b77b6b00a7e4701e58dcc72fd2452c32bf
Arthur Tainmont <arthurtainmont@Macbookair.local>
2024-04-25 20:31:43 +0200

    skip all dirs besides obsidian, add specific changed files and push once at end

--- autoSync.py
+++ autoSync.py
@@ -18,16 +18,17 @@ while True:
     os.system('cd ..; git pull')
     time.sleep(600)  # Wait for 10 min before checking again
     for root, dirs, files in os.walk(directory):
-        if ".git" or ".obsidian" in root:
+        if root != "../Obsidian":
             continue
         for filename in files:
+            #print("   " + filename)
             path = os.path.join(root, filename)
             modified_time = os.path.getmtime(path)
             if modified_time != initial_times.get(path):
                 changes = True
-                print(f"{path} has been modified!")
-                os.system(f'cd ..; git add {path}')
+                print(f"'{path[3:]}' has been modified!")
+                os.system(f'cd ..; git add "{path[3:]}"')
                 initial_times[path] = modified_time
     if changes:
-         os.system('git commit -m "autosync"; git push')
+         os.system('cd ..; git pull; git commit -m "autosync"; git push')
     
