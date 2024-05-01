14bb295f3211145466cb1ab0530a8d6161aeeb81
Arthur Tainmont <53705599+artainmo@users.noreply.github.com>
2024-05-02 00:48:02 +0200

    Update autoSync.py

--- autoSync.py
+++ autoSync.py
@@ -5,30 +5,38 @@ directory = "../"
 
 # Store the initial file modification times
 initial_times = {}
+current_files = {}
 for root, dirs, files in os.walk(directory):
-    if ".git" in root:
+    if root != "../Obsidian":
         continue
     for filename in files:
         path = os.path.join(root, filename)
         initial_times[path] = os.path.getmtime(path)
+        current_files[path] = True # Remember all the files to verify if some got removed
 
 # Continuously check for changes
 while True:
     changes = False
+    current_files = { file:False for file in current_files }
     os.system('cd ..; git pull')
     time.sleep(600)  # Wait for 10 min before checking again
     for root, dirs, files in os.walk(directory):
         if root != "../Obsidian":
             continue
         for filename in files:
-            #print("   " + filename)
             path = os.path.join(root, filename)
+            current_files[path] = True
             modified_time = os.path.getmtime(path)
             if modified_time != initial_times.get(path):
+                # 'dict.get(unknownKey)' returns None when key is unknown while 'dict[unknownKey]' would create an execution error.
+                # Thus new files are considered as modified files who need to be pushed.
                 changes = True
-                print(f"'{path[3:]}' has been modified!")
+                print(f"\033[1;33m'{path[3:]}' has been modified!\033[0m") if initial_times.get(path) != None else print(f"\033[0;32m'{path[3:]}' has been created!\033[0m")
                 os.system(f'cd ..; git add "{path[3:]}"')
                 initial_times[path] = modified_time
+    for file, still_present in current_files.items():
+        if !still_present:
+            os.system(f'cd ..; git rm "{file[3:]}"')
     if changes:
          os.system('cd ..; git pull; git commit -m "autosync"; git push')
     
